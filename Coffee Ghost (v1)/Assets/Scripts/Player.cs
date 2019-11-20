f179d6fb1730515e2a7e95d41e0f934ce239daeb
colinsoguero <49736223+colinsoguero@users.noreply.github.com>
2019-11-20 13:43:39 -0700

    Player Movement
    Player movement and basic scene

---
+++ Coffee Ghost (v1)/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 10.0f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15F);
+
+
+        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+
+    }
+}
+
