bc451125677ef59daa8cdceadf2f8405e95d0daa
Jake Henson <jakegatsby935@gmail.com>
2019-11-24 00:02:17 -0700

    Added assets, layout, fixed movement
    Updated with table assets, fixed movement on player (still a little wonky but it should be okay) 
    Updated map.

--- Coffee Ghost (v1)/Assets/Scripts/Player.cs
+++ Coffee Ghost (v1)/Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed = 10.0f;
+    public float rotation_speed = 0.15f;  //parameterized this so we can change it later 
 
     void Start()
     {
@@ -17,11 +18,18 @@ public class Player : MonoBehaviour
         float moveVertical = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15F);
+
+        //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15F);
+
+        if(moveHorizontal != 0 || moveVertical != 0){ //if player is moving
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), rotation_speed);
+        }
+        else{ //otherwise they're standing still 
+            transform.rotation = Quaternion.Slerp(transform.rotation, transform.rotation, rotation_speed);
+        }
 
 
         transform.Translate(movement * speed * Time.deltaTime, Space.World);
-
     }
 }
 
