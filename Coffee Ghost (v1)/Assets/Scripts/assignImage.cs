7c7fb12c4078ae61f144e9cdfc659bb5f818ad7b
hai li <hayu7527@colorado.edu>
2019-12-16 19:01:00 -0700

    assignimage
    screams

---
+++ Coffee Ghost (v1)/Assets/Scripts/assignImage.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class assignImage : MonoBehaviour
+{
+     private AssignCoffee otherscript;
+     public Sprite foam;
+     public Sprite black;
+     public Sprite milk;
+
+     // Start is called before the first frame update
+     void Start()
+     {
+
+     }
+ 
+     // Update is called once per frame
+     void Update()
+     {
+         if (otherscript.cust1 == "foam")
+         {
+             SetFoam();
+             
+         }
+         if (otherscript.cust1 == "black")
+         {
+             Debug.Log("black");
+             SetBlack();
+             
+         }
+         if (otherscript.cust1 == "milk")
+         {
+             Debug.Log("milk");
+             SetMilk();
+         }
+ 
+     }
+ 
+     public void SetFoam()
+     {
+         Debug.Log("foamed");
+         gameObject.GetComponent<SpriteRenderer>().sprite = foam;
+     }
+ 
+     public void SetMilk()
+     {
+          gameObject.GetComponent<SpriteRenderer>().sprite = milk;
+     }
+     public void SetBlack()
+     {
+         gameObject.GetComponent<SpriteRenderer>().sprite = black;
+     }
+ }
